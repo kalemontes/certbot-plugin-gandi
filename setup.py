3bd0ac84ad9797f4235b70691b28184ce1b81991
Camilo Montes <ale8689@gmail.com>
2020-12-15 13:52:41 +0100

    Changes to make SNAP build work

--- setup.py
+++ setup.py
@@ -1,29 +1,49 @@
-from setuptools import setup, find_packages 
+from distutils.version import LooseVersion
+import os
+import sys
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+from setuptools import __version__ as setuptools_version
+from setuptools import find_packages
+from setuptools import setup
+
+version = '1.2.5'
+
+# Remember to update local-oldest-requirements.txt when changing the minimum
+# acme/certbot version.
+install_requires = [
+    'setuptools',
+    'zope.interface',
+    'requests>=2.4.2',
+]
+
+if not os.environ.get('SNAP_BUILD'):
+    install_requires.extend([
+        'acme>=0.31.0',
+        'certbot>=1.1.0',
+    ])
+elif 'bdist_wheel' in sys.argv[1:]:
+    raise RuntimeError('Unset SNAP_BUILD when building wheels '
+                       'to include certbot dependencies.')
+if os.environ.get('SNAP_BUILD'):
+    install_requires.append('packaging')
+
+setuptools_known_environment_markers = (LooseVersion(setuptools_version) >= LooseVersion('36.2'))
+if setuptools_known_environment_markers:
+    install_requires.append('mock ; python_version < "3.3"')
+elif 'bdist_wheel' in sys.argv[1:]:
+    raise RuntimeError('Error, you are trying to build certbot wheels using an old version '
+                       'of setuptools. Version 36.2+ of setuptools is required.')
+elif sys.version_info < (3,3):
+    install_requires.append('mock')
 
 setup(
     name='certbot-plugin-gandi',
-    version='1.2.5',
+    version=version,
     author="Yohann Leon",
     author_email="yohann@leon.re",
     description="Certbot plugin for authentication using Gandi LiveDNS",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
     url="https://github.com/obynio/certbot-plugin-gandi",
-    packages=find_packages(),
     python_requires=' >=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
-    install_requires=[
-        'certbot',
-        'zope.interface',
-        'requests>=2.4.2',
-    ],
-    entry_points={
-        'certbot.plugins': [
-            'dns = certbot_plugin_gandi.main:Authenticator',
-        ],
-    },
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Plugins',
@@ -45,4 +65,14 @@ setup(
         'Topic :: System :: Systems Administration',
         'Topic :: Utilities',
         ],
+
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=install_requires,
+    entry_points={
+        'certbot.plugins': [
+            'dns-gandi = certbot_plugin_gandi.main:Authenticator',
+        ],
+    },
+
 )
